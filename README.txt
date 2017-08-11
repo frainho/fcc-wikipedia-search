94ce6fefa3e1672fa2d19362a0c0f14a9c65fe1a
Filipe Rainho <frainho@fneves.userzoom.com>
2017-08-11 22:37:36 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/ncoder92/pen/VzPRmg.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $("#triggerSearch").click(function() {
+    $("#data").empty();
+    var a = $("#searchItem").val();
+    $.ajax({
+      type: "GET",
+      url: "https://en.wikipedia.org/w/api.php",
+      data: { action: "opensearch", search: a, format: "json" },
+      dataType: "jsonp",
+      contentType: "application/json; charset=utf-8",
+      success: function(result) {
+        $(result[1]).each(function(i, value) {
+          $("#data").append(
+            "<p class='articleStyling'><b>" +
+              value +
+              "</b><br>" +
+              result[2][i] +
+              "<br><a href='" +
+              result[3][i] +
+              "'> Go to article</a></p>"
+          );
+        });
+      }
+    });
+  });
+  $("#triggerRandom").click(function() {
+    $.ajax({
+      type: "GET",
+      url: "https://en.wikipedia.org/w/api.php",
+      data: {
+        action: "query",
+        generator: "random",
+        grnnamespace: "0",
+        prop: "extracts",
+        exchars: "500",
+        indexpageids: "",
+        format: "json"
+      },
+      dataType: "jsonp",
+      contentType: "application/json; charset=utf-8",
+      success: function(result) {
+        var article = window.open(
+          "https://en.wikipedia.org/?curid=" + result.query.pageids[0]
+        );
+      }
+    });
+  });
+});
